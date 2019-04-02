2370e4a193fd76233f978790773887028ba7cf42
ryou <visualjupiter@gmail.com>
2019-04-02 21:28:24 +0900

    fix: 記事にGitHubのスタイルを適用

---
+++ components/Article.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="markdown-body" v-html="html" />
+</template>
+
+<script>
+import 'github-markdown-css'
+
+export default {
+  props: {
+    html: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.markdown-body {
+  color: inherit;
+}
+</style>
